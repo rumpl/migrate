e417daae98023eaed2841c7dc501ca8c1c539764
Djordje <lukic.djordje@gmail.com>
2013-03-05 00:30:49 +0100

    Some unit tests

--- test/index.js
+++ test/index.js
@@ -1,10 +1,37 @@
-var assert = require("assert");
+var assert = require("should");
+var Schema = require('../lib/database/schema');
 
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
-            assert.equal(-1, [1,2,3].indexOf(5));
-            assert.equal(-1, [1,2,3].indexOf(0));
+describe('Migration', function () {
+    var schema;
+
+    beforeEach(function (done) {
+        schema = new Schema;
+        done();
+    });
+
+    describe('Schema', function () {
+        it('should add a table', function () {
+            schema.createTable('test', function () {
+            });
+            assert.exist(schema.tables.test);
+        });
+
+        it('should throw if asked to create the same table twice', function () {
+            (function () {
+                schema.createTable('test', function () {
+                });
+                schema.createTable('test', function () {
+                });
+            }).should.throw();
+        });
+    });
+
+    describe('Table', function () {
+        it('should add a column', function () {
+            schema.createTable('test', function (table) {
+                table.addColumn('test', 'int');
+            });
+            assert.exist(schema.tables.test.columns.test);
         });
     });
 });
