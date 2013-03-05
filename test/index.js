2f3321c1b61f9dc16517a459ad8fc5f24de0f20a
Djordje <lukic.djordje@gmail.com>
2013-03-05 01:29:24 +0100

    More unit tests

--- test/index.js
+++ test/index.js
@@ -1,15 +1,37 @@
 var assert = require("should");
 var Schema = require('../lib/database/schema');
+var Table = require('../lib/database/table');
 
 describe('Migration', function () {
-    var schema;
 
-    beforeEach(function (done) {
-        schema = new Schema;
-        done();
+    describe('Table', function () {
+        var table;
+
+        beforeEach(function () {
+            table = new Table;
+        });
+
+        it('should add a column', function () {
+            table.addColumn('test', 'int');
+            assert.exist(table.columns.test);
+        });
+
+        it('should throw if asked to add the same column twice', function () {
+            (function () {
+                table.addColumn('test', 'int');
+                table.addColumn('test', 'string');
+            }).should.throw();
+        })
     });
 
     describe('Schema', function () {
+        var schema;
+
+        beforeEach(function (done) {
+            schema = new Schema;
+            done();
+        });
+
         it('should add a table', function () {
             schema.createTable('test', function () {
             });
@@ -23,15 +45,6 @@ describe('Migration', function () {
                 schema.createTable('test', function () {
                 });
             }).should.throw();
-        });
-    });
-
-    describe('Table', function () {
-        it('should add a column', function () {
-            schema.createTable('test', function (table) {
-                table.addColumn('test', 'int');
-            });
-            assert.exist(schema.tables.test.columns.test);
         });
     });
 });
