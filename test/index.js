843b1b9a60cba628cb2fae44861c1d358bd8db11
Djordje Lukic <lukic.djordje@gmail.com>
2013-03-13 14:44:05 +0100

    Indentation to 2 spaces

--- test/index.js
+++ test/index.js
@@ -4,55 +4,55 @@ var Table = require('../lib/database/table');
 
 describe('Migration', function () {
 
-    describe('Table', function () {
-        var table;
+  describe('Table', function () {
+    var table;
 
-        beforeEach(function () {
-            table = new Table;
-        });
-
-        it('should add a column', function () {
-            table.addColumn('test', 'int');
-            assert.exist(table.columns.test);
-        });
-
-        it('should throw if asked to add the same column twice', function () {
-            (function () {
-                table.addColumn('test', 'int');
-                table.addColumn('test', 'string');
-            }).should.throw();
-        });
+    beforeEach(function () {
+      table = new Table;
     });
 
-    describe('Schema', function () {
-        var schema;
-
-        beforeEach(function (done) {
-            schema = new Schema;
-            done();
-        });
-
-        it('should add a table', function () {
-            schema.addTable('test', function () {
-            });
-            assert.exist(schema.addedTables.test);
-        });
-
-        it('should throw if asked to create the same table twice', function () {
-            (function () {
-                schema.addTable('test', function () {
-                });
-                schema.addTable('test', function () {
-                });
-            }).should.throw();
-        });
-
-        it('should add a primary key', function () {
-            schema.addTable('test', function (table) {
-                table.addColumn('id', 'int', {'primary': true});
-
-            });
-            schema.addedTables.test.columns.id.primary.should.be.true;
-        });
+    it('should add a column', function () {
+      table.addColumn('test', 'int');
+      assert.exist(table.columns.test);
     });
+
+    it('should throw if asked to add the same column twice', function () {
+      (function () {
+        table.addColumn('test', 'int');
+        table.addColumn('test', 'string');
+      }).should.throw();
+    });
+  });
+
+  describe('Schema', function () {
+    var schema;
+
+    beforeEach(function (done) {
+      schema = new Schema;
+      done();
+    });
+
+    it('should add a table', function () {
+      schema.addTable('test', function () {
+      });
+      assert.exist(schema.addedTables.test);
+    });
+
+    it('should throw if asked to create the same table twice', function () {
+      (function () {
+        schema.addTable('test', function () {
+        });
+        schema.addTable('test', function () {
+        });
+      }).should.throw();
+    });
+
+    it('should add a primary key', function () {
+      schema.addTable('test', function (table) {
+        table.addColumn('id', 'int', {'primary': true});
+
+      });
+      schema.addedTables.test.columns.id.primary.should.be.true;
+    });
+  });
 });
